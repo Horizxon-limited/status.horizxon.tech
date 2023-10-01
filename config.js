ea60560192b565e706cc67ba1ea4fc790eec28a6
Draxler <76139625+HenGPlayZ@users.noreply.github.com>
2023-10-01 10:20:32 +0700

    Update config.js

--- config.js
+++ config.js
@@ -2,7 +2,7 @@ module.exports = {
   nodes: [
      {
       host: "lava.horizxon.tech",
-      password: "horizxon.xyz",
+      password: "horizxon.tech",
       port: 80,
       retryDelay: 1,
       identifier: "lava.horizxon.tech・(NO-SSL)",
@@ -11,7 +11,7 @@ module.exports = {
 
      {
       host: "lava1.horizxon.tech",
-      password: "horizxon.xyz",
+      password: "horizxon.tech",
       port: 443,
       retryDelay: 1,
       identifier: "lava1.horizxon.tech・(SSL)",
@@ -29,7 +29,7 @@ module.exports = {
 
     {
       host: "lava3.horizxon.tech",
-      password: "horizxon.xyz",
+      password: "horizxon.tech",
       port: 443,
       retryDelay: 1,
       identifier: "lava3.horizxon.tech・(SSL)",
